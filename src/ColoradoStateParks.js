606a26325544f60659dd752dea15d98814de6c38
Achraf Mehanneche <achrafmehenneche.am@gmail.com>
2023-09-11 15:55:03 +0100

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,40 @@
 import React from "react";
+import {trees, wildlife} from "./parks/RockyMountain";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+import {
+  trees as parkTrees,
+  wildlife as parkWildlife,
+} from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  console.log(trees); 
+  console.log(RMFunctions.trees);
+  console.log(parkTrees);
 
-  return <h1>Colorado State Parks!</h1>;
+  RMFunctions.wildlife();
+
+  RMFunctions.elevation();
+
+  parkWildlife();
+
+  wildlife();
+  howManyParks(); // => "42 parks!" 
+
+
+
+
+
+  return (
+    <div>
+
+      <MesaVerde />
+      
+    </div>
+    // <h1>Colorado State Parks!</h1>;
+  )
 }
+
+export default ColoradoStateParks;
